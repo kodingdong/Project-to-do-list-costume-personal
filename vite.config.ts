e45f4db7e0d9fd0dcd895336102b6a67badd8f54
Aji wirawan <ajiwirawan2@gmail.com>
2026-06-07 07:47:29 +0700

    chore(deps): hapus elysia tailwind dan package-lock

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
-import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
 	plugins: [
-		tailwindcss(),
 		sveltekit(),
 		SvelteKitPWA({
 			registerType: 'autoUpdate',
